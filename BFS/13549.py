ce38c6296d7ec0385c593afcc7f65bb3db588186
bottle-honey <honey_sheep@naver.com>
2022-08-08 13:13:01 +0900

    #13549#2

--- BFS/13549.py
+++ BFS/13549.py
@@ -7,12 +7,18 @@ queue.put((0,n))
 
 while queue:
     count,cur_pos = queue.get()
-    if cur_pos == k:
-        result = count
-        break
     if cur_pos-1 >=0:
+        if cur_pos+1 == k:
+            result = count+1
+            break
+        elif cur_pos-1 ==k:
+            result = count+1
+            break
         queue.put((count+1,cur_pos+1))
         queue.put((count+1,cur_pos-1))
     if cur_pos*2 > 0 and cur_pos*2 < 2*k:
+        if cur_pos*2 == k:
+            result = count
+            break
         queue.put((count,cur_pos*2))
-print(count)
+print(result)
