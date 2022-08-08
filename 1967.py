e6990b66f1590514f36fb1e62c437eab48848503
bottle-honey <honey_sheep@naver.com>
2022-08-09 07:41:26 +0900

    #1967#3

--- 1967.py
+++ 1967.py
@@ -1,6 +1,4 @@
 #트리의 지름
-
-
 def dfs(start_node,tree):
     stk = [(start_node,0)]
     result = 0
@@ -8,9 +6,8 @@ def dfs(start_node,tree):
     while stk:
         node,cur_v = stk.pop()
         visited[node] = True
-        node_inf = tree[node]
         #i : 인접 노드 / n : 간선 거리
-        for i,n in enumerate(node_inf):
+        for i,n in enumerate(tree[node]):
             if n != 0:
                 if not visited[i]:
                     stk.append((i,cur_v+n))
@@ -22,12 +19,15 @@ n = int(input())
 
 tree = [[0]*n for _ in range(n)]
 
+result = 0
+
 for i in range(n-1):
     a,b,c = map(int,input().split())
     tree[a-1][b-1] = c
     tree[b-1][a-1] = c
 
-result = 0
+
 for i in range(n):
     result = max(result,dfs(i,tree))
+    
 print(result)
