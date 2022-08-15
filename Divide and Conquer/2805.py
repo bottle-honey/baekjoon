5cd491913eaaee577a43d166a97696af3a742d1b
bottle-honey <honey_sheep@naver.com>
2022-08-16 04:41:10 +0900

    #2805#2

--- Divide and Conquer/2805.py
+++ Divide and Conquer/2805.py
@@ -5,20 +5,30 @@ input = sys.stdin.readline
 n,m=map(int,input().split())
 
 trees = list(map(int,input().split()))
-
-start = 1
+trees.sort()
+start = 0
 end = max(trees)
 result = 0
 while start<=end:
-    sum = 0
+    count = 0
+    
     # mid : h : 절단기의 높이
     mid = (start+end)//2
-    for tree in trees:
-        if tree > mid:
-            sum += tree-mid
-    if sum >= m:
+    start_t = 0
+    end_t = n-1
+    min_idx = n-1
+    while start_t <= end_t:
+        mid_t = (start_t + end_t)//2
+        if trees[mid_t] >= mid:
+            min_idx = min(min_idx,mid_t)
+            start_t = mid_t+1
+        else:
+            end_t = mid_t-1
+    count = sum(trees[min_idx:]) - ((n-min_idx) * mid)
+    if count >= m:
         result = max(result,mid)
         start = mid + 1
     else:
         end = mid - 1
+
 print(result)
