8c4070cae7e008bd1662449c4100f44b6f0918bc
bottle-honey <honey_sheep@naver.com>
2022-08-16 07:35:11 +0900

    #2589#4

--- DFS/2589.py
+++ DFS/2589.py
@@ -1,7 +1,6 @@
 #보물섬
 import sys
 from collections import deque
-import copy
 input = sys.stdin.readline
 n,m = map(int,input().split())
 
@@ -10,29 +9,27 @@ for _ in range(n):
     map.append(list(input().rstrip('\n')))
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
-result = [0]
 
-
+result = 0
         
 for i in range(n):
     for j in range(m):
         if map[i][j] == 'L':
+            visited = [[False]*m for _ in range(n)]
+            board = [[0]*m for _ in range(n)]
             #BFS
-            map_copy = copy.deepcopy(map)
-            q = deque([(i,j,0)])
+            q = deque([(i,j)])
             while q:
-                x,y,distance = q.pop()
-                map_copy[x][y] = distance
-                able = 0
+                x,y = q.popleft()
+                visited[x][y] = True
+                result = max(result,board[x][y])
                 for d in range(len(dx)):
                     nx = x+dx[d]
                     ny = y+dy[d]
-                    if nx <0 or nx >=n or ny <0 or ny >=m or map_copy[nx][ny] != 'L':
+                    if nx <0 or nx >=n or ny <0 or ny >=m or map[nx][ny] != 'L' or visited[nx][ny]:
                         continue
-                    able+=1
-                    q.appendleft((nx,ny,distance+1))
-                if able == 0:
-                    result.append(distance)
+                    q.append((nx,ny))
+                    board[nx][ny] = board[x][y]+1
                     
 
-print(max(result))
+print(result)
