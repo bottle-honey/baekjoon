620b84434353f5e53c5735b2b7d3db1fa4f99dcb
bottle-honey <honey_sheep@naver.com>
2022-08-16 07:08:23 +0900

    #2589#2

--- DFS/2589.py
+++ DFS/2589.py
@@ -1,5 +1,7 @@
 #보물섬
 import sys
+from collections import deque
+
 input = sys.stdin.readline
 n,m = map(int,input().split())
 
@@ -8,27 +10,24 @@ for _ in range(n):
     map.append(list(input().rstrip('\n')))
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
-result = 0
+result = [0]
+
+def bfs(x,y,distance):
+    map_copy[x][y] = distance
+    result.append(distance)
+    for d in range(len(dx)):
+        nx = x+dx[d]
+        ny = y+dy[d]
+        if nx <0 or nx >=n or ny <0 or ny >=m or map_copy[nx][ny] != 'L':
+            continue
+        bfs(nx,ny,distance+1)
+        
 for i in range(n):
     for j in range(m):
-        #dfs
         if map[i][j] == 'L':
-            stk = [(i,j,0)]
-            visited = [[False]*m for _ in range(n)]
-            
-            while stk:
-                x,y,distance = stk.pop()
-                visited[x][y] = True
-                # 종착지 판별 변수
-                able = 0
-                for d in range(len(dx)):
-                    nx= x+dx[d]
-                    ny=y+dy[d]
-                    if nx<0 or nx >= n or ny < 0 or ny >= m or visited[nx][ny]:
-                        continue
-                    if map[nx][ny] == 'L':
-                        stk.append((nx,ny,distance+1))
-                        able+=1
-                if able == 0:
-                    result = max(result,distance)
-print(result)
+            #BFS
+            map_copy = map[:]
+            bfs(i,j,1)
+
+
+print(max(result))
