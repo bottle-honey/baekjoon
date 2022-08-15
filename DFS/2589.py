4685fe8b7dbb69ce65edf7e1dc014be04f1120fb
bottle-honey <honey_sheep@naver.com>
2022-08-16 07:22:37 +0900

    #2589#3

--- DFS/2589.py
+++ DFS/2589.py
@@ -1,7 +1,7 @@
 #보물섬
 import sys
 from collections import deque
-
+import copy
 input = sys.stdin.readline
 n,m = map(int,input().split())
 
@@ -12,22 +12,27 @@ dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 result = [0]
 
-def bfs(x,y,distance):
-    map_copy[x][y] = distance
-    result.append(distance)
-    for d in range(len(dx)):
-        nx = x+dx[d]
-        ny = y+dy[d]
-        if nx <0 or nx >=n or ny <0 or ny >=m or map_copy[nx][ny] != 'L':
-            continue
-        bfs(nx,ny,distance+1)
+
         
 for i in range(n):
     for j in range(m):
         if map[i][j] == 'L':
             #BFS
-            map_copy = map[:]
-            bfs(i,j,1)
-
+            map_copy = copy.deepcopy(map)
+            q = deque([(i,j,0)])
+            while q:
+                x,y,distance = q.pop()
+                map_copy[x][y] = distance
+                able = 0
+                for d in range(len(dx)):
+                    nx = x+dx[d]
+                    ny = y+dy[d]
+                    if nx <0 or nx >=n or ny <0 or ny >=m or map_copy[nx][ny] != 'L':
+                        continue
+                    able+=1
+                    q.appendleft((nx,ny,distance+1))
+                if able == 0:
+                    result.append(distance)
+                    
 
 print(max(result))
