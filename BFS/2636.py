5eec922e9d1bf6e81016e6c79114ee829a23b697
bottle-honey <honey_sheep@naver.com>
2022-08-17 06:23:13 +0900

    #2636#2

--- BFS/2636.py
+++ BFS/2636.py
@@ -1,7 +1,6 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
-import copy 
 n,m = map(int,input().split())
 
 board = []
@@ -14,22 +13,27 @@ result = 0
 time = 0
 def bfs(x,y):
     q = deque([(x,y)])
-    copy_board = copy.deepcopy(board)
-    copy_board[x][y] = 2
+    visited = [[False]*m for _ in range(n)]
+    space = [(x,y)]
     while q:
         x,y = q.popleft()
+        visited[x][y] = True
         for d in range(4):
                 nx = x + dx[d]
                 ny = y + dy[d]
-                if nx<0 or nx >=n or ny <0 or ny >=m:
+                if nx==0 or nx ==n-1 or ny ==0 or ny ==m-1:
                     return []
-                if copy_board[nx][ny] == 0:
+                if board[nx][ny] == 0 and not visited[nx][ny]:
                     q.append((nx,ny))
-                    copy_board[x][y] = 2
-    return copy_board
+    return space
 while True:
     count = 0
     remove = []
+    space_list = []
+    for i in range(1,n-1):
+        for j in range(1,m-1):
+            space_list= space_list + bfs(i,j)
+            
     for x in range(1,n-1):
         for y in range(1,m-1):
             if board[x][y] == 1:
@@ -39,9 +43,8 @@ while True:
                     nx = x + dx[d]
                     ny = y + dy[d]
                     if board[nx][ny] == 0:
-                        copy_board = bfs(nx,ny)
-                        if copy_board:
-                            board= copy_board[:]
+                        if (nx,ny) in space_list:
+                            continue                            
                         else:
                             air+=1
                             break
@@ -49,6 +52,9 @@ while True:
                     remove.append((x,y))
     for x,y in remove:
         board[x][y] = 0
+    # print(space_list)
+    # for i in range(n):
+    #     print(board[i])
     
     if count == 0:
         break
