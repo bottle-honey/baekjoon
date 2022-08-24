23ffb53df7ff7e3aa576d7e5db030629d35323ea
bottle-honey <honey_sheep@naver.com>
2022-08-24 11:02:44 +0900

    #12865#2

--- Dynammic_Programming/12865.py
+++ Dynammic_Programming/12865.py
@@ -8,10 +8,18 @@ array = []
 for _ in range(n):
     x,y = map(int,input().split())
     array.append((x,y))
-array.sort()
-dp = [0]*(k+1)
-for w,v in array:
-    for idx in range(w,len(dp)):
-        dp[idx] = max(dp[idx-w]+v,dp[idx])
+dp = [[0]*(k+1) for _ in range(n)]
+for i in range(n):
+    w,v = array[i]
+    for j in range(k+1):
+        if i-1>=0:
+            if j-w>=0:
+                dp[i][j] = max(dp[i-1][j],dp[i-1][j-w]+v)
+            else:
+                dp[i][j] = dp[i-1][j]
+        else:
+            if j-w>=0:
+                dp[i][j] = v
 
-print(dp[k])
+print(dp[n-1][k])
+
