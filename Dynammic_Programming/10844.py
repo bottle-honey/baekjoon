4f591f37051452c8dee45ea48258f48b4079364d
bottle-honey <honey_sheep@naver.com>
2022-08-29 13:27:30 +0900

    #10844#2

--- Dynammic_Programming/10844.py
+++ Dynammic_Programming/10844.py
@@ -5,11 +5,16 @@ input = sys.stdin.readline
 DIV = 1000000000
 n = int(input())
 
-dp = [(1,2,6)]
+dp =[[0]*10 for _ in range(n)]
+dp[0] = [0] + [1]*9
 
-for i in range(1,n):
-    a = dp[i-1][1]
-    b = dp[i-1][0]
-    c = dp[i-1][1] + dp[i-1][2]*2
-    dp.append((a,b,c))
+for i in range(n-1):
+    for idx in range(10):
+        if idx == 0:
+            dp[i+1][1] += dp[i][idx]
+        elif idx == 9:
+            dp[i+1][8] += dp[i][idx]
+        else:
+            dp[i+1][idx+1] += dp[i][idx]
+            dp[i+1][idx-1] += dp[i][idx]
 print(sum(dp[n-1])%DIV)
